cf6d5d4aebdcd9de424b981cfc8bb65f71a85eb3
TshiamoMed <25506218@sun.ac.za>
2024-08-26 17:16:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -183,6 +183,6 @@ const User = {
     }
 };
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(3001, () => {
+    console.log('Server is running on http://localhost:3001');
 });
