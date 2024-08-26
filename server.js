c8043a6fb2593ffeac8b3f51c819ca46b48506f3
TshiamoMed <25506218@sun.ac.za>
2024-08-26 16:39:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -27,13 +27,21 @@ app.use(session({
 
 require('dotenv').config();
 
-const connection = mysql.createConnection({
+const MySQLStore = require('express-mysql-session')(session);
+const sessionStore = new MySQLStore({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
+
+app.use(session({
+    secret: 'secret_key',
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: true
+}));
 
 connection.connect((err) => {
     if (err) {
