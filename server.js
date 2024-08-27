586622d0ce4bc83933cece72b8a19f6a5632b6d8
TshiamoMed <25506218@sun.ac.za>
2024-08-27 09:08:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -51,24 +51,6 @@ connection.connect((err) => {
     console.log('DB server connected successfully');
 });
 
-// Create the 'users' table if it doesn't exist
-const createTableQuery = `
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    full_name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-);
-`;
-
-connection.query(createTableQuery, (err, results) => {
-    if (err) {
-        console.error('Error creating table:', err.stack);
-        return;
-    }
-    console.log('Table created or already exists');
-});
 
 // Root route to redirect to register page
 app.get('/', (request, response) => {
